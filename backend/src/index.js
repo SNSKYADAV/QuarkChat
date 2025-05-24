e89d8ca6ca2bdb46c90591050cb036f2fc3afc30
SNSKYADAV <106435330+SNSKYADAV@users.noreply.github.com>
2025-05-24 19:23:43 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
  //   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
  // });
 
-    app.get("/*", (req,res) => {
+    app.get("*", (req,res) => {
       res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 }
